344acee34e8821644fa25646317afc32016df2db
slhczz <slhczz@126.com>
2019-05-10 23:02:16 +0800

    0.0.01

--- src/main/java/com/sy/stu/school/SchoolApplication.java
+++ src/main/java/com/sy/stu/school/SchoolApplication.java
@@ -1,13 +1,30 @@
 package com.sy.stu.school;
 
+import org.hibernate.validator.HibernateValidator;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
 
 @SpringBootApplication
+@MapperScan("com.sy.stu.school.dao")
 public class SchoolApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SchoolApplication.class, args);
+    }
+    @Bean
+    public Validator validator(){
+        ValidatorFactory validatorFactory = Validation.byProvider(HibernateValidator.class).configure()
+                .addProperty( "hibernate.validator.fail_fast", "true" )
+                .buildValidatorFactory();
+        Validator validator = validatorFactory.getValidator();
+
+        return validator;
     }
 
 }
